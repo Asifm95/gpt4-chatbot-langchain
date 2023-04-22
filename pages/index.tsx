e295bc3878188a122450316dc994df17040c76e7
Asifm95 <asifm2882@gmail.com>
2023-04-22 21:45:30 +0400

    ref: remove title

--- pages/index.tsx
+++ pages/index.tsx
@@ -132,9 +132,9 @@ export default function Home() {
     <>
       <Layout>
         <div className="mx-auto flex flex-col gap-4">
-          <h1 className="text-2xl font-bold leading-[1.1] tracking-tighter text-center">
+          {/* <h1 className="text-2xl font-bold leading-[1.1] tracking-tighter text-center">
             Chat With Your Legal Docs
-          </h1>
+          </h1> */}
           <main className={styles.main}>
             <div className={styles.cloud}>
               <div ref={messageListRef} className={styles.messagelist}>
@@ -276,9 +276,7 @@ export default function Home() {
           </main>
         </div>
         <footer className="m-auto p-4">
-          <a href="https://twitter.com/mayowaoshin">
-            Powered by LangChainAI. Demo built by Mayo (Twitter: @mayowaoshin).
-          </a>
+          <a href="https://twitter.com/mayowaoshin">Powered by LangChainAI.</a>
         </footer>
       </Layout>
     </>
